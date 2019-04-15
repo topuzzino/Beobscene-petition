89bba0a893fac2f9a69f80fc9b43899f522c7989
Evgenia <topuzzino@hotmail.de>
2019-04-15 10:46:00 +0200

    one more fix in getProfileInfo

--- utils/db.js
+++ utils/db.js
@@ -40,10 +40,11 @@ exports.removeProfile = function removeProfile(userId) {
 
 //
 exports.getProfileInfo = function getProfileInfo(userId) {
-    let q = `SELECT * FROM users
-    LEFT JOIN user_profiles
+    let q = `SELECT users.id, users.first, users.last, users.email, age, city, url, obscene
+    FROM users
+    LEFT JOIN user_profiles 
     ON user_profiles.userid = users.id
-    WHERE userid = $1`;
+    WHERE users.id = $1`;
     return db.query(q, [userId]);
 };
 
